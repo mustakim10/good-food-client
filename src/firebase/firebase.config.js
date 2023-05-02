c09f40ad2b629ec122cb9a20cb41a90ba35dd5d4
Mustakim <sycospike1@gmail.com>
2023-05-02 12:21:06 +0600

    added firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAD3UB0zECBrpPhRpYgSQsSqFaz7EjqjRA",
+  authDomain: "good-food-24989.firebaseapp.com",
+  projectId: "good-food-24989",
+  storageBucket: "good-food-24989.appspot.com",
+  messagingSenderId: "27929128416",
+  appId: "1:27929128416:web:060acffd14257aad1ecf85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
